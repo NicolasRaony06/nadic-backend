ec50e5ecc027351366874c3e51b6f611b78f6bed
NicolasRaony06 <nicolas.raony06@gmail.com>
2025-06-27 14:38:35 -0300

    Add: view e template cadastro de produto

--- projeto-crm/crm/views.py
+++ projeto-crm/crm/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Produto
+from decimal import Decimal
 
-def home(request):
-    return HttpResponse('Olá mundo')
+def cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'produto_cadastro.html')
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        descricao = request.POST.get('descricao')
+        preco = request.POST.get('preco')
+        qtd_estoque = request.POST.get('qtd_estoque')
+        ativo = request.POST.get('ativo')
+
+        try:
+            produto = Produto.objects.create(
+                nome = nome,
+                descricao = descricao,
+                preco = Decimal(preco),
+                qtd_estoque = int(qtd_estoque),
+                ativo = ativo
+            )
+
+            produto.save()
+        except:
+            print("Erro \n")
+            return redirect('cadastro_produto')
+
+        return redirect('cadastro_produto')
