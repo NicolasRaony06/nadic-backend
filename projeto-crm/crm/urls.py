45e1e1e290d9b9e3db1b066e8cd3a1ba8ed4090b
NicolasRaony06 <nicolas.raony06@gmail.com>
2025-06-27 23:09:23 -0300

    Add: view e template editar produto

--- projeto-crm/crm/urls.py
+++ projeto-crm/crm/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('cadastro/', views.cadastro, name='cadastro_produto'),
+    path('editar/<int:id>/', views.editar_produto, name='editar_produto'),
 ]
--- projeto-crm/crm/views.py
+++ projeto-crm/crm/views.py
@@ -27,4 +27,29 @@ def cadastro(request):
         except:
             add_message(request, constants.ERROR, "Erro ao tentar cadastrar o produto!")
             return redirect('cadastro_produto')
+        
+        add_message(request, constants.SUCCESS, "Produto cadastrado com sucesso!")
+        return redirect('cadastro_produto')
+    
+def editar_produto(request, id):
+    if request.method == 'GET':
+        produto = Produto.objects.get(id=id)
+        return render(request, "produto_editar.html", {'produto': produto})
+    
+    if request.method == 'POST':
+        produto = Produto.objects.get(id=id)
+
+        try:
+            produto.nome = request.POST.get('nome') if request.POST.get('nome') else produto.nome
+            produto.descricao = request.POST.get('descricao') if request.POST.get('descricao') else produto.descricao
+            produto.preco = request.POST.get('preco') if request.POST.get('preco') else produto.preco
+            produto.qtd_estoque = request.POST.get('qtd_estoque') if request.POST.get('qtd_estoque') else produto.qtd_estoque
+            produto.ativo = request.POST.get('ativo') if request.POST.get('ativo') else produto.ativo
+
+            produto.save()
+        except:
+            add_message(request, constants.ERROR, "Erro ao tentar atualizar produto!")
+            return redirect('cadastro_produto')
+            
+        add_message(request, constants.SUCCESS, "Produto atualizado com sucesso!")
         return redirect('cadastro_produto')
